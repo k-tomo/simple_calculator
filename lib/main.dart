5c7058395f6f475704b6fe8f0e073acace9bc751
kadoyahirotomo <tomo577@icloud.com>
2023-10-10 22:45:48 +0900

    コメントを追加しました．

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'view/calculator_screen.dart';
+/*
+このアプリはRiverpodとfreezedの使い方を学ぶためのチュートリアルです．
+ */
 
 void main() {
   runApp(
@@ -12,11 +15,10 @@ void main() {
 
 class SimpleCalculator extends StatelessWidget {
   const SimpleCalculator({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Simple Calculator',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
